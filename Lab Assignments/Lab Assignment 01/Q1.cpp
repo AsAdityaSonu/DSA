0185fbcfed8946e34298b102ba0078e31e9b2d5e
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2023-08-02 12:14:09 +0530

    file 1 added

---
+++ Lab Assignments/Lab Assignment 01/Q1.cpp
@@ -0,0 +1,38 @@
+/* Develop a Menu driven program to demonstrate the following operations of Arrays 
+-——MENU——-
+1.CREATE
+2. DISPLAY
+3. INSERT
+4. DELETE
+5. LINEAR SEARCH
+6. EXIT
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[]={1,2,3,4,5};
+    int opt;
+    cout<<"----------------------------------------------------------------\n";
+    cout<<"Choose any of the following options:\n"<<endl;
+    cout<<"1.Create\n2.Display\n3.Insert\n4.Delete\n5.LINEAR SEARCH\n6.Exit\n";
+    cin>>opt;
+
+    switch (opt)
+    {
+    case 1:
+
+        break;
+    
+    case 2:
+
+        break;
+
+    case 3:
+
+        break;
+    default:
+        break;
+    }
+}
