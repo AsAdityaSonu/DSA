164dd0b95abd62a63ed93b60340cdff986bd3746
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2023-09-22 15:07:16 +0530

    interleaving using queue program updated

--- Lab Assignments/Lab Assignment 04/Q3.cpp
+++ Lab Assignments/Lab Assignment 04/Q3.cpp
@@ -4,7 +4,43 @@
 using namespace std;
 
 void interleave(queue<int> *q){
+    if (q->size() % 2 != 0)
+        cout << "Input even number of integers." << endl;
+ 
     
+    stack<int> s;
+    int halfSize = q->size() / 2;
+ 
+    // into the stack
+    for (int i = 0; i < halfSize; i++) {
+        s.push(q->front());
+        q->pop();
+    }
+ 
+    // enqueue back the stack elements
+    while (!s.empty()) {
+        q->push(s.top());
+        s.pop();
+    }
+ 
+    // dequeue the first half elements of queue
+    for (int i = 0; i < halfSize; i++) {
+        q->push(q->front());
+        q->pop();
+    }
+ 
+
+    for (int i = 0; i < halfSize; i++) {
+        s.push(q->front());
+        q->pop();
+    }
+ 
+    while (!s.empty()) {
+        q->push(s.top());
+        s.pop();
+        q->push(q->front());
+        q->pop();
+    }
 }
 
 int main()
